15084c40292a2291403c39c35fe748f9f710affa
Cleavon <cleavon.design@gmail.com>
2026-08-20 03:09:17 +0100

    Debug stripe key

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,19 @@
 import Stripe from "stripe";
 
 
+console.log(
+  "STRIPE KEY EXISTS:",
+  !!process.env.STRIPE_SECRET_KEY
+);
+
+
+console.log(
+  "STRIPE KEY START:",
+  process.env.STRIPE_SECRET_KEY?.substring(0,10)
+);
+
+
+
 export const stripe = new Stripe(
 
   process.env.STRIPE_SECRET_KEY!
