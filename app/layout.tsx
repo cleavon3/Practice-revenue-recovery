fd8b04075aa5a7eeaa34e86ff7a81110afdeb08c
Cleavon <cleavon.design@gmail.com>
2026-09-08 21:12:49 +0100

    Add Meta Pixel tracking setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Revenue Recovery Calculator",
+  title: "Practice Revenue Recovery",
   description:
     "Calculate the potential revenue opportunity from missed patient enquiries.",
 };
@@ -13,71 +13,82 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+  const googleAdsId = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID;
+
   return (
     <html lang="en">
       <body>
         {children}
 
-        {/* Meta Pixel */}
+        {/* META PIXEL */}
 
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
+        {pixelId && (
+          <>
+            <Script id="meta-pixel" strategy="afterInteractive">
+              {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function()
+                {n.callMethod?
+                n.callMethod.apply(n,arguments):
+                n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;
+                n.push=n;
+                n.loaded=!0;
+                n.version='2.0';
+                n.queue=[];
+                t=b.createElement(e);
+                t.async=!0;
+                t.src=v;
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}
+                (window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
 
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function()
-{n.callMethod?
-n.callMethod.apply(n,arguments):
-n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;
-n.push=n;
-n.loaded=!0;
-n.version='2.0';
-n.queue=[];
-t=b.createElement(e);
-t.async=!0;
-t.src=v;
-s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}
-(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${pixelId}');
+                fbq('track', 'PageView');
+              `}
+            </Script>
 
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+                alt=""
+              />
+            </noscript>
+          </>
+        )}
 
-fbq('init',
-'${process.env.NEXT_PUBLIC_META_PIXEL_ID}'
-);
+        {/* GOOGLE ADS */}
 
+        {googleAdsId && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${googleAdsId}`}
+              strategy="afterInteractive"
+            />
 
-fbq('track','PageView');
+            <Script id="google-ads" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
 
-`}
-        </Script>
+                function gtag(){
+                  dataLayer.push(arguments);
+                }
 
-        {/* Google Ads Tag */}
+                gtag('js', new Date());
 
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}`}
-          strategy="afterInteractive"
-        />
-
-        <Script id="google-ads" strategy="afterInteractive">
-          {`
-
-window.dataLayer = window.dataLayer || [];
-
-function gtag(){
-dataLayer.push(arguments);
-}
-
-gtag('js', new Date());
-
-
-gtag(
-'config',
-'${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}'
-);
-
-`}
-        </Script>
+                gtag(
+                  'config',
+                  '${googleAdsId}'
+                );
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   );
